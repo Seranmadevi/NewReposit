2d583945746f7d2f1081c4eaa57467dcabf2dee5
PraveenSQA <pravinkrish143@gmail.com>
2022-08-26 16:04:53 +0530

    first

---
+++ src/samplepackage/SampleClass.java
@@ -0,0 +1,27 @@
+package samplepackage;
+
+public class SampleClass {
+
+	public static void main(String[] args) 
+	{
+		byte num1=99;
+		
+		byte 123rtyu;
+		
+		byte _abi;
+		byte $tyui;
+		
+		byte abi;
+		byte ABI;
+		
+		byte number_1;
+		
+		
+		
+		
+		sysout
+		
+		System.out.println("num1 value is "+num1);
+	}
+
+}
